d741bca6dba1a3007303454e47d63462e938e7c2
Jess Ryu <42489770+Hyeonji-Ryu@users.noreply.github.com>
2020-12-05 23:36:19 +0900

    Update and rename serving_code to serving_code.py

--- MNIST_CPU/serving_code.py
+++ MNIST_CPU/serving_code.py
@@ -3,12 +3,12 @@
 
 # download docker image
 
-```dockerfile
+```docker
 docker pull tensorflow/serving
 ```
 
 # make serving server
 
-```dockerfile
+```docker
 docker run -p 8501:8501 --mount type=bind,source=$PWD/'modelfile',target=/models/'model_name' -e MODEL_NAME='model_name' -td tensorflow/serving
 ```
